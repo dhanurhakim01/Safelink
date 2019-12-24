a94b8d8d8f647f1b4303cf7ffa85730a63c485d1
Codelistnull <51483446+dhanurhakim01@users.noreply.github.com>
2019-12-24 12:23:41 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -1,6 +1,8 @@
-//<![CDATA[
 var setting = {
 exceptionurl : "google.com,twitter.com,facebook.com,plus.google.com,pinterest.com,linkedin.com,komiku.samehadakunettv.com,whatsapp.com,samehadakuhome.blogspot.com",
 path : "#?o="
 };
-//]]>
+
+function downloadJSAtOnload(){var n=document.createElement("script");n.src="https://cdn.jsdelivr.net/gh/Arlina-Design/phantom@master/antiboomclick.js",document.body.appendChild(n)}window.addEventListener?window.addEventListener("load",downloadJSAtOnload,!1):window.attachEvent?window.attachEvent("onload",downloadJSAtOnload):window.onload=downloadJSAtOnload;
+
+var lazyadsense=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyadsense||0!=document.body.scrollTop&&!1===lazyadsense)&&(function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyadsense=!0)},!0);
